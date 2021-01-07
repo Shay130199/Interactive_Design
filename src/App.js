af9e4bbd70ad7f4ae1b582773aa2169ac96edaab
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2021-01-07 20:55:37 +0000

    Buttons

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../src/assets/logo.png";
+import title from "../src/assets/boost-me.png";
+
+
+
+function App( ) {
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+
+export default App;
